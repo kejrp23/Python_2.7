0b634d1e5c557544576bbd0691e4d7e5b4516871
Jason Pittman <j.pittman51@att.net>
2016-02-24 21:57:37 -0500

    Update Addressbook.py

--- Addressbook.py
+++ Addressbook.py
@@ -1,3 +1,7 @@
+# Created By: Jason R. Pittman
+# Created On: 2/23/2016
+# Wriiten in: Python 2.7
+
 """
 This program is a simple address book program.
 It is something that I could use for work or life.
@@ -6,8 +10,6 @@ I am looking for but this will also help me with my skills
 as I am learning Python right now. 
 """
 
-# Created By: Jason R. Pittman
-# Created On: 2/23/2016
-# Wriiten in: Python 2.7
+
 
 
