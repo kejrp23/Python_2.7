df769ef0b8e463ddf57df13a59bf4915a46965a6
Jason Pittman <j.pittman51@att.net>
2016-02-24 21:56:53 -0500

    Update Addressbook.py

--- Addressbook.py
+++ Addressbook.py
@@ -1,1 +1,13 @@
+"""
+This program is a simple address book program.
+It is something that I could use for work or life.
+I have programs that will provide me the solution that
+I am looking for but this will also help me with my skills
+as I am learning Python right now. 
+"""
+
+# Created By: Jason R. Pittman
+# Created On: 2/23/2016
+# Wriiten in: Python 2.7
+
 
