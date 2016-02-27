16b95e2d1b644461dbb877bfa22ddd5eb55b42e9
Jason Pittman <j.pittman51@att.net>
2016-02-27 11:31:18 -0500

    Update Addressbook.py

--- Addressbook.py
+++ Addressbook.py
@@ -1,15 +1,23 @@
 # Created By: Jason R. Pittman
 # Created On: 2/23/2016
-# Wriiten in: Python 2.7
+# Wriiten for: Python 2.7
 
 """
 This program is a simple address book program.
 It is something that I could use for work or life.
 I have programs that will provide me the solution that
 I am looking for but this will also help me with my skills
-as I am learning Python right now. 
+as I am learning Python. 
+
 """
 
 
+From datetime import datetime
+From time import sleep
 
+print "initializing program................"
+sleep(3)
+
+
+print " Welcome to Address Book 1.0 "
 
