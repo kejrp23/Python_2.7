27694a7f6ee46e6b9f9591237fc40a792d5717f9
Jason Pittman <j.pittman51@att.net>
2016-04-02 18:20:28 -0400

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -15,13 +15,13 @@ The program should never terminate unless the user decides to exit
 """
 #This is the main structural requirements for the Calendar
 from time import sleep,strftime #save time and import both at once
-#MY_FIRST_NAME = "Jason" #at some point let the user do this through raw_input()
+MY_FIRST_NAME = "Jason" #at some point let the user do this through raw_input()
 calendar = {} #dictionaries use values and pairs so we will use Dates and events
 #Above here we have the time elements a Consant name to work with and a dictionary
 #ok from here on is the Calendar
 def welcome():#This is the Splash screen or Welcome area for getting started.
   #This is a possible line for adding a raw_input for the users name
-  MY_FIRST_NAME = raw_input("Lets Get Started\n Please enter your first name: ")
+  #MY_FIRST_NAME = raw_input("Lets Get Started\n Please enter your first name: ")
   print "Welcome " + MY_FIRST_NAME + "."
   print "Opening your Calendar one moment please"
   sleep(2)
@@ -32,8 +32,40 @@ def welcome():#This is the Splash screen or Welcome area for getting started.
 def start_calendar():
   welcome()
   start = True
+  #The Calendar is below here
   while start:#keeps the program running unless exited by choice
     user_choice = raw_input('A to add, U to Update, V to View, D to Delete, X to Exit: ')
     user_choice = user_choice.upper()
-    #check these indentions i don't think they are right
-#   	if user_choice == 'V':
+    #Adding the Choices they should all be similiar
+    if user_choice == 'V':
+      if len(calendar.keys()) < 1: # this checks to see if anything is on the calendar by searching the calendar dictionary keys to see if anything is on them
+        print "Calendar Empty"
+      else:
+        print calendar
+    # Choice two - update
+    elif user_choice == 'U':
+      date = int(raw_input('Enter Date (MM/DD/YYYY): '))
+      update = raw_input('Enter the update: ')
+      #add the inputed changes
+      calendar[date] = update
+      print 'Calendar Successfully Updated'
+      print calendar
+    # Choice three - Add
+    elif user_choice == 'A':
+      event = raw_input('Enter Event: ')
+      date = int(raw_input('Enter Date (MM/DD/YYYY): '))
+      if (len(date) > 10):
+      
+                 
+      
+                 
+      
+        
+
+        
+        
+        
+        
+        
+        
+start_calendar() #launcher for debugging
