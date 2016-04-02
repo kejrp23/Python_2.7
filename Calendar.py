97dde1959bfb2ecac44ed52736f0569d211eb109
Jason Pittman <j.pittman51@att.net>
2016-04-02 17:07:29 -0400

    Update Calendar.py
    updated to current progress level. 
    added a few more comments. 
    added a line for the user to input their specific name commented it out 
    for later addition.

--- Calendar.py
+++ Calendar.py
@@ -15,28 +15,25 @@ The program should never terminate unless the user decides to exit
 """
 #This is the main structural requirements for the Calendar
 from time import sleep,strftime #save time and import both at once
-MY_FIRST_NAME = "Jason" #at some point let the user do this through raw_input()
+#MY_FIRST_NAME = "Jason" #at some point let the user do this through raw_input()
 calendar = {} #dictionaries use values and pairs so we will use Dates and events
 #Above here we have the time elements a Consant name to work with and a dictionary
 #ok from here on is the Calendar
 def welcome():#This is the Splash screen or Welcome area for getting started.
   #This is a possible line for adding a raw_input for the users name
+  MY_FIRST_NAME = raw_input("Lets Get Started\n Please enter your first name: ")
   print "Welcome " + MY_FIRST_NAME + "."
   print "Opening your Calendar one moment please"
-  sleep(1)
+  sleep(2)
   print "Today's date is: " + strftime("%A %B %d,%Y") 
-  print "The Current time is " + strftime("%I:%M:%S")
+  print "the Current time is " + strftime("%I:%M:%S")
   print "What would you like to do? "
 #time to set how the calendar runs until exit  
 def start_calendar():
   welcome()
   start = True
   while start:#keeps the program running unless exited by choice
-    user_choice = raw_input('A to Add an Event, U to Update and Existing Event, V to View an Event, D to Delete an Event, X to Exit the program: ')#This can probably be cleaned up to make more sense and take up less space. it's currently so long it's on two lines
+    user_choice = raw_input('A to add, U to Update, V to View, D to Delete, X to Exit: ')
     user_choice = user_choice.upper()
     #check these indentions i don't think they are right
-  if user_choice == 'V':
-      if len(calender.keys()) < 1:
-      	print " The Calendar is Empty right now..."
-        
-start_calendar()
+#   	if user_choice == 'V':
